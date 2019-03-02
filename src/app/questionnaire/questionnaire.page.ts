7ca75776ec330eb53c73ace0ce647a75cf512a0c
Maxime Delsart <PQX0EzHluHXszJwt>
2019-03-02 16:47:59 +0100

    First Commit

---
+++ src/app/questionnaire/questionnaire.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questionnaire',
+  templateUrl: './questionnaire.page.html',
+  styleUrls: ['./questionnaire.page.scss'],
+})
+export class QuestionnairePage implements OnInit {
+
+  constructor() { }
+
+  items: Array<string>;
+
+  ngOnInit() {
+    this.setItems();
+  }
+
+  setItems() {
+    this.items = ['Baincthum', 'Boulogne sur Mer', 'Condette', 'Conteville-les-Boulogne', 'Dannes', 'Echinghen', 'Equihen plage', 'Hesdigneul-les-Boulogne', 'Hesdin l\'abbé', 'Isques', 'La Cappelle les Boulogne'];
+  }
+
+  filterItems(ev: any) {
+    this.setItems();
+    let val = ev.target.value;
+
+    if (val && val.trim() !== '') {
+      this.items = this.items.filter(function(item) {
+        return item.toLowerCase().includes(val.toLowerCase());
+      });
+    }
+  }
+  question3autre(ev: any) {
+    let val = ev.target.value;
+  }
+
+}
