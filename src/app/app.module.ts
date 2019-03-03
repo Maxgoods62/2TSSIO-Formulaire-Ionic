2afaeb3b9f859559386cc261487b01e77bbc8548
Maxime Delsart <PQX0EzHluHXszJwt>
2019-03-03 16:28:20 +0100

    Recupère des valeurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
 export class AppModule {}
--- src/app/questionnaire/questionnaire.page.ts
+++ src/app/questionnaire/questionnaire.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'app-questionnaire',
@@ -7,16 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionnairePage implements OnInit {
 
-  constructor() { }
-
   items: Array<string>;
+
+  public questionnaireForm: FormGroup;
+
+
+
+  constructor(public formBuilder: FormBuilder) {
+    this.questionnaireForm = this.formBuilder.group({
+      question1: [''],
+      question2: [''],
+      question3: [''],
+      question4a: [''],
+      question4b: [''],
+      question5: [''],
+      question6: [''],
+      question7: [''],
+      question8: [''],
+      question9: [''],
+      question10: [''],
+      question11: [''],
+      question12: [''],
+      question13: [''],
+      question14: [''],
+      question15: [''],
+      question16: [''],
+      question17: [''],
+      question18: [''],
+    })
+  }
+
 
   ngOnInit() {
     this.setItems();
   }
 
   setItems() {
-    this.items = ['Baincthum', 'Boulogne sur Mer', 'Condette', 'Conteville-les-Boulogne', 'Dannes', 'Echinghen', 'Equihen plage', 'Hesdigneul-les-Boulogne', 'Hesdin l\'abbé', 'Isques', 'La Cappelle les Boulogne', 'Le Portel', 'Nesles', 'Neufchâtel Hardelot', 'Outreau', 'Pernes-les-Boulognes', 'Pittefaux', 'St Etienne-au-mont', 'St Léonard', 'St-Martin-Boulogne', 'Wimereux', 'Wimille'];
+    this.items = ['Baincthum', 'Boulogne sur Mer', 'Condette', 'Conteville-les-Boulogne', 'Dannes', 'Echinghen', 'Equihen plage', 'Hesdigneul-les-Boulogne', 'Hesdin l\'abbé', 'Isques', 'La Capelle les Boulogne', 'Le Portel', 'Nesles', 'Neufchâtel Hardelot', 'Outreau', 'Pernes-les-Boulognes', 'Pittefaux', 'St Etienne-au-mont', 'St Léonard', 'St-Martin-Boulogne', 'Wimereux', 'Wimille'];
   }
 
   filterItems(ev: any) {
@@ -24,17 +52,72 @@ export class QuestionnairePage implements OnInit {
     let val = ev.target.value;
 
     if (val && val.trim() !== '') {
-      this.items = this.items.filter(function(item) {
+      this.items = this.items.filter(function (item) {
         return item.toLowerCase().includes(val.toLowerCase());
       });
     }
   }
-  question3autre(ev: any) {
-    let val = ev.target.value;
+
+
+
+  sendQuestionnaire() {
+    console.log(this.questionnaireForm.value)
   }
 
-  question6autre(ev: any) {
-    let val = ev.target.value;
+
+  questionValue(question, event) {
+
+    this.questionnaireForm.controls[question].setValue(event.detail.value);
+
+    console.log(question + " : " + event.detail.value);
   }
+
+  checkboxValue(question, event) {
+    let valueBase = this.questionnaireForm.value[question];
+
+    let valueSend = new Array();
+
+    if (valueBase != []) {
+      valueBase.forEach(function (element) {
+        valueSend.push(element);
+      });
+    }
+
+    if (event.detail.checked) {
+      // ajouter 
+      valueSend.push(event.detail.value)
+      //console.log(valueSend);
+      this.questionnaireForm.controls[question].setValue(valueSend);
+    } else {
+      // enlever
+      let pos = valueSend.indexOf(event.detail.value);
+      valueSend.splice(pos, 1)
+      this.questionnaireForm.controls[question].setValue(valueSend);
+    }
+    console.log(question + " : " + valueSend);
+  }
+
+  question6autre(event) {
+
+    let valueBase = this.questionnaireForm.value["question6"];
+    let valueSend = new Array();
+
+    if (valueBase != []) {
+      valueBase.forEach(function (element) {
+        if (element == "BAFA" || element == "BNSSA" || element == "PSC1-AFGSU" || element == "PSE1-PSE1" || element == "TOEIC") {
+          valueSend.push(element);
+        }
+      });
+    }
+
+    // Ajouter dans le tableau
+    let valeur = event.detail.value;
+    valueSend.push(valeur);
+
+    this.questionnaireForm.controls["question6"].setValue(valueSend);
+    console.log(valueSend);
+  }
+
+
 
 }
